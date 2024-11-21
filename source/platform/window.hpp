c902f6ab17e196d1406d1c1adcac6c5283ac1271
Chris DeJong <magiktrikdev@gmail.com>
2024-11-21 12:28:19 -0600

    Window base class implemented, basic-blocking style.

--- source/platform/window.hpp
+++ source/platform/window.hpp
@@ -3,15 +3,19 @@
 #include <string>
 #include <definitions.hpp>
 
+#define MAGWINDEF_WIDTH     1600
+#define MAGWINDEF_HEIGHT    900
+#define MAGWINDEF_TITLE     "Magnitude Rendering Utility"
+
 // --- Window ------------------------------------------------------------------
 //
 // A window, as you might imagine, opens a viewport that can be interacted with.
 // Windows generally don't do anything special other than maintain and modify the
-// state of the window. As such, rendering doesn't occur with windows, rather the
-// renderer uses the window to render to.
+// state of the window.
 //
-// Since a window is typically platform independent, we delegate the implementation
-// of the window to the platform it is defined on.
+// You don't need to trigger anything special for any platform, given the platform
+// has a window implementation; just call Window::create() to get back a window
+// suitable for the platform you're running on.
 //
 
 class Window
@@ -23,14 +27,13 @@ class Window
         virtual void            poll_events()           = 0;
         virtual void            swap_frames()           = 0;
         virtual bool            should_close() const    = 0;
+        virtual void            show()                  = 0;
 
         virtual void            set_size(i32 width, i32 height)     = 0;
-        virtual void            set_position(i32 x, i32 y)          = 0;
-        virtual void            set_minimized(b32 minimize)         = 0;
         virtual void            set_maximized(b32 maximize)         = 0;
         virtual void            set_visibility(b32 visible)         = 0;
         virtual void            set_title(const std::string& title) = 0;
-        virtual void            set_borderless(b32 enable)          = 0;
+        virtual void            set_borderless(b32 enable, i32 exit_width, i32 exit_height) = 0;
 
         virtual std::string     get_title()     const = 0;
         virtual i32             get_width()     const = 0;
@@ -40,6 +43,9 @@ class Window
         virtual bool            is_visible()    const = 0;
         virtual bool            is_borderless() const = 0;
         virtual bool            is_focused()    const = 0;
+
+        virtual bool            did_size_change()   const = 0;
+        virtual bool            did_focus_change()  const = 0;
 
 };
 
