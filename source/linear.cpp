1083a3cf1f2fd879795111c2a5025634f3e7029b
Chris DeJong <magiktrikdev@gmail.com>
2024-11-29 08:37:18 -0600

    Linear algebra math library, core vectors.

--- source/linear.cpp
+++ source/linear.cpp
@@ -5,6 +5,18 @@
 // Implementation of vector 2D operations.
 //
 
+r32& vector2::
+operator[](i32 idx)
+{
+    return this->elements[idx];
+}
+
+r32 vector2::
+operator[](i32 idx) const
+{
+    return this->elements[idx];
+}
+
 vector2& vector2::
 operator+=(const vector2& rhs)
 {
@@ -105,10 +117,53 @@ operator/(const r32& lhs, const vector2& rhs)
 
 }
 
+bool
+operator==(const vector2& lhs, const vector2& rhs)
+{
+
+    bool result = (lhs[0] == rhs[0] &&
+                   lhs[1] == rhs[1]);
+    return result;
+
+}
+
+std::ostream& 
+operator<<(std::ostream& os, const vector2& rhs)
+{
+
+    os << rhs.x << " " << rhs.y;
+    return os;
+
+}
+
+std::string 
+to_string(const vector2& rhs)
+{
+
+    std::string result;
+    result += rhs.x;
+    result += " ";
+    result += rhs.y;
+    return result;
+
+}
+
 // --- Vector 3D ---------------------------------------------------------------
 //
 // Implementation of vector 3D operations.
 //
+
+r32& vector3::
+operator[](i32 idx)
+{
+    return this->elements[idx];
+}
+
+r32 vector3::
+operator[](i32 idx) const
+{
+    return this->elements[idx];
+}
 
 vector3& vector3::
 operator+=(const vector3& rhs)
@@ -214,10 +269,56 @@ operator/(const r32& lhs, const vector3& rhs)
 
 }
 
+bool
+operator==(const vector3& lhs, const vector3& rhs)
+{
+
+    bool result = (lhs[0] == rhs[0] &&
+                   lhs[1] == rhs[1] &&
+                   lhs[2] == rhs[2]);
+    return result;
+
+}
+
+std::ostream& 
+operator<<(std::ostream& os, const vector3& rhs)
+{
+
+    os << rhs.x << " " << rhs.y << " " << rhs.z;
+    return os;
+
+}
+
+std::string 
+to_string(const vector3& rhs)
+{
+
+    std::string result;
+    result += rhs.x;
+    result += " ";
+    result += rhs.y;
+    result += " ";
+    result += rhs.z;
+    return result;
+
+}
+
 // --- Vector 3D Homogenous ----------------------------------------------------
 //
 // The bread and butter of computer graphics: three-dimensional homogenous vectors.
 //
+
+r32& vector4::
+operator[](i32 idx)
+{
+    return this->elements[idx];
+}
+
+r32 vector4::
+operator[](i32 idx) const
+{
+    return this->elements[idx];
+}
 
 vector4& vector4::
 operator+=(const vector4& rhs)
@@ -325,6 +426,87 @@ operator/(const r32& lhs, const vector4& rhs)
 
     vector4 result = rhs;
     result /= lhs;
+    return result;
+
+}
+
+bool
+operator==(const vector4& lhs, const vector4& rhs)
+{
+
+    bool result = (lhs[0] == rhs[0] &&
+                   lhs[1] == rhs[1] &&
+                   lhs[2] == rhs[2] &&
+                   lhs[3] == rhs[3]);
+    return result;
+
+}
+
+r32 vector4::
+magnitude_squared() const
+{
+
+    r32 result = this->elements[0] * this->elements[0] +
+                 this->elements[1] * this->elements[1] +
+                 this->elements[2] * this->elements[2];
+    return result;
+
+}
+
+r32 vector4::
+magnitude() const
+{
+
+    r32 result = this->elements[0] * this->elements[0] +
+                 this->elements[1] * this->elements[1] +
+                 this->elements[2] * this->elements[2];
+    return sqrtf(result);
+
+}
+
+vector4 vector4::
+normalize() const
+{
+
+    r32 magnitude = this->magnitude();
+    vector4 result = *this;
+    result /= magnitude;
+    return result;
+
+}
+
+vector4 vector4::
+homogenize() const
+{
+
+    vector4 result = *this;
+    result /= result.w;
+    result.w = 1.0f;
+    return result;
+
+}
+
+std::ostream& 
+operator<<(std::ostream& os, const vector4& rhs)
+{
+
+    os << rhs.x << " " << rhs.y << " " << rhs.z << " " << rhs.w;
+    return os;
+
+}
+
+std::string 
+to_string(const vector4& rhs)
+{
+
+    std::string result;
+    result += rhs.x;
+    result += " ";
+    result += rhs.y;
+    result += " ";
+    result += rhs.z;
+    result += " ";
+    result += rhs.w;
     return result;
 
 }
